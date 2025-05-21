806e90372166b8d1694782f3339eae825eb1e7ef
basis64computer <basis64computer@gmail.com>
2025-05-21 15:29:27 +0800

    Add files via upload

--- js/initialize.js
+++ js/initialize.js
@@ -449,6 +449,73 @@ function messageModal(msg) {
     showModal();
 }
 
+function callbackMessageModal(msg, callback) {
+  document.getElementById('messageModal').innerHTML = `<div class="relative p-4 w-full max-w-2xl max-h-full">
+        <!-- Modal content -->
+        <div class="relative bg-white rounded-lg shadow-sm dark:bg-neutral-700">
+            <!-- Modal header -->
+            <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600 border-gray-200">
+                <h3 class="text-xl font-semibold text-gray-900 dark:text-white" data-i18n="messages.info">
+                    Info
+                </h3>
+                <button type="button" onclick="${callback.name}()" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white">
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                    </svg>
+                    <span class="sr-only">Close</span>
+                </button>
+            </div>
+            <!-- Modal body -->
+            <div class="p-4 md:p-5 space-y-4">
+                <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">${msg}</p>
+            </div>
+            <!-- Modal footer -->
+            <!-- Modal footer -->
+<div class="w-full flex justify-end p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+  <button type="button" onclick="${callback.name}()" class="py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-neutral-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" data-i18n="messages.close">
+    Tutup
+  </button>
+</div>
+
+        </div>
+    </div>`;
+    showModal();
+}
+
+function confirmModal(title, msg, button, callback) {
+  document.getElementById('messageModal').innerHTML = `<div class="relative p-4 w-full max-w-2xl max-h-full">
+        <!-- Modal content -->
+        <div class="relative bg-white rounded-lg shadow-sm dark:bg-neutral-700">
+            <!-- Modal header -->
+            <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600 border-gray-200">
+                <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                    ${title}
+                </h3>
+                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-dismiss-modal="true">
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                    </svg>
+                    <span class="sr-only">Close</span>
+                </button>
+            </div>
+            <!-- Modal body -->
+            <div class="p-4 md:p-5 space-y-4">
+                <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">${msg}</p>
+            </div>
+            <!-- Modal footer -->
+            <!-- Modal footer -->
+<div class="w-full flex justify-end p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+  <button data-dismiss-modal="true" type="button" class="py-2.5 px-5 me-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-neutral-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" data-i18n="messages.close">
+    Tutup
+  </button>
+  <button type="button" class="py-2.5 px-5 text-sm font-medium text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onclick="${callback.name}();">${button}</button>
+</div>
+
+        </div>
+    </div>`;
+    showModal();
+}
+
 function photoModal(img) {
   document.getElementById('messageModal').innerHTML = `
   <div class="absolute right-0 top-0 p-4">
@@ -480,6 +547,11 @@ function loadingModal(msg) {
     <!-- Loading Text -->
     <p class="text-lg font-semibold text-gray-700">${msg}</p>
   </div>`;
+    showModal();
+}
+
+function customModal(layout) {
+  document.getElementById('messageModal').innerHTML = layout;
     showModal();
 }
 
